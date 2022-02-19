b7570345eb20c5412679b02fa7b26e3a06eb927e
Anna Lu <anna.lu.yufeng@gmail.com>
2022-02-19 20:40:02 +0800

    feat: add CountryCard component

---
+++ src/pages/Home/components/CountryCard/CountryCard.jsx
@@ -0,0 +1,53 @@
+import { string, number } from "prop-types";
+import { Link } from "react-router-dom";
+
+import styles from "./CountryCard.module.scss";
+
+function CountryCard({ alpha3Code, capital, flag, name, population, region }) {
+  const details = [
+    {
+      label: "Population",
+      value: population.toLocaleString(), // TODO: move to data transform
+    },
+    {
+      label: "Region",
+      value: region,
+    },
+    {
+      label: "Capital",
+      value: capital,
+    },
+  ];
+  return (
+    <Link to={`/country/${alpha3Code}`} className={styles.root}>
+      <img
+        src={flag}
+        alt={`country flag of ${name}`}
+        loading="lazy"
+        width={265}
+      />
+      <div className={styles.content}>
+        <h3 className={styles.title}>{name}</h3>
+        <ul className={styles.details}>
+          {details.map(({ label, value }) => (
+            <li key={label}>
+              <b>{label}</b>
+              {value}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Link>
+  );
+}
+
+CountryCard.propTypes = {
+  alpha3Code: string.isRequired,
+  capital: string.isRequired,
+  flag: string.isRequired,
+  name: string.isRequired,
+  population: number.isRequired,
+  region: string.isRequired,
+};
+
+export default CountryCard;
