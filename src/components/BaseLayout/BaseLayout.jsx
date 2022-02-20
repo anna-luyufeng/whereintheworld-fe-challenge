e3b676d66f010514fe0728ac039e85f894b96015
Anna Lu <anna.lu.yufeng@gmail.com>
2022-02-20 20:44:03 +0800

    feat: light/dark mode toggler

--- src/components/BaseLayout/BaseLayout.jsx
+++ src/components/BaseLayout/BaseLayout.jsx
@@ -1,11 +1,40 @@
-import { node } from "prop-types";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
-// import LightModeIcon from "@mui/icons-material/LightMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import IconButton from "@mui/material/IconButton";
 
 import styles from "./BaseLayout.module.scss";
 
 export default function BaseLayout({ children }) {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  useEffect(() => {
+    const preferredTheme = localStorage.getItem("theme");
+
+    const systemPreferredDarkTheme =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (preferredTheme) return setIsDarkTheme(preferredTheme === "dark");
+
+    setIsDarkTheme(systemPreferredDarkTheme);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
+
+    if (isDarkTheme) {
+      document.querySelector("html").classList.remove("theme-light");
+      document.querySelector("html").classList.add("theme-dark");
+    } else {
+      document.querySelector("html").classList.add("theme-light");
+      document.querySelector("html").classList.remove("theme-dark");
+    }
+  }, [isDarkTheme]);
+
+  const toggleDarkTheme = () => setIsDarkTheme(!isDarkTheme);
+
   return (
     <>
       <header className={styles.header}>
@@ -13,7 +42,13 @@ export default function BaseLayout({ children }) {
           <Link to="/">
             <h1 className={styles.title}>Where in the world?</h1>
           </Link>
-          <DarkModeIcon />
+          <IconButton onClick={toggleDarkTheme}>
+            {isDarkTheme ? (
+              <LightModeIcon sx={{ color: "white" }} />
+            ) : (
+              <DarkModeIcon sx={{ color: "var(--text)" }} />
+            )}
+          </IconButton>
         </div>
       </header>
       <main className={styles.main}>
@@ -22,7 +57,3 @@ export default function BaseLayout({ children }) {
     </>
   );
 }
-
-BaseLayout.propTypes = {
-  children: node.isRequired,
-};
