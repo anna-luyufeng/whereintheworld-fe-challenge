9b92eb2ebfc5fbbde2216f2371fd2e5655099e46
Anna Lu <anna.lu.yufeng@gmail.com>
2022-02-21 14:52:50 +0800

    chore: use 8 items for home skeleton

--- src/components/Skeleton/HomeCountryCards.jsx
+++ src/components/Skeleton/HomeCountryCards.jsx
@@ -5,7 +5,7 @@ import styles from "pages/Home/Home.module.scss";
 function HomeCountryCards() {
   return (
     <div className={styles.cards}>
-      {[...Array(4).keys()].map((item) => (
+      {[...Array(8).keys()].map((item) => (
         <Skeleton key={item} variant="rectangular" width={265} height={340} />
       ))}
     </div>
